9340ec4f84f85290136708f577dcb12efdffb76b
Swarnali Saha <swarnali.saha@globalids.com>
2024-10-29 14:06:37 +0530

    pattern 2 done

---
+++ pattern_2.js
@@ -0,0 +1,23 @@
+/*
+==================================
+print 
+*
+**
+***
+****
+*****
+==================================
+*/
+
+const nForest = function(n){
+    for(let i=0; i<n; i++){
+        let line = "";
+        for(let j=0; j<=i; j++){
+            line +="*"
+        }
+        console.log(line)
+    }
+}
+
+let rows = 5;
+nForest(rows)
